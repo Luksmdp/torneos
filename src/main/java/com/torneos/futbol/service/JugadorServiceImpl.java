373236cf750b7198ce66af5e7ce8091d7537a18f
Luksmdp <rivarola.mdq@gmail.com>
2024-02-14 21:19:39 +0100

    Se arreglo la funcion save de JugadorServiceImpl

--- src/main/java/com/torneos/futbol/service/JugadorServiceImpl.java
+++ src/main/java/com/torneos/futbol/service/JugadorServiceImpl.java
@@ -1,7 +1,9 @@
 package com.torneos.futbol.service;
 
 import com.torneos.futbol.model.dto.JugadorDto;
+import com.torneos.futbol.model.entity.Equipo;
 import com.torneos.futbol.model.entity.Jugador;
+import com.torneos.futbol.repository.EquipoRepository;
 import com.torneos.futbol.repository.JugadorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,14 +16,22 @@ import java.util.List;
 public class JugadorServiceImpl implements JugadorService {
 
     private final JugadorRepository jugadorRepository;
+    private final EquipoRepository equipoRepository;
 
     @Override
     public Jugador save(JugadorDto jugadorDto) {
         Jugador jugador = new Jugador();
         jugador.setEdad(jugadorDto.getEdad());
-        jugador.setEquipo(null);
         jugador.setNombre(jugadorDto.getNombre());
         jugador.setPosicion(jugadorDto.getPosicion());
+
+        Integer equipoId = jugadorDto.getEquipoId();
+
+        if(equipoId != null && equipoRepository.existsById(equipoId)){
+            Equipo equipo = equipoRepository.findById(equipoId).orElse(null);
+            jugador.setEquipo(equipo);
+        }
+
         return jugadorRepository.save(jugador);
     }
 
